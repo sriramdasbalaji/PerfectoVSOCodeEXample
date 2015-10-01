8d1ab14f5b2bb9c6fe02adc17bdfc95c861ed3da
uzie <PerfectoMobile256@gmail.com>
2015-09-30 21:23:08 -0400

    config file poit the VSO

--- src/test/java/util.java
+++ src/test/java/util.java
@@ -235,8 +235,8 @@ public class util {
 		System.out.println("EXECUET TEST BUILD THE LIST FROM THE FILE ");
 		BufferedReader br;
 		try {
-			//File f = new File("..\\..\\..\\testConfigFiles\\config1.txt");
-			File f = new File("C:\\aaa\\MS\\BuildAgent\\_work\\testConfigFiles\\config1.txt");
+			File f = new File("..\\..\\..\\testConfigFiles\\config1.txt");
+			//File f = new File("C:\\aaa\\MS\\BuildAgent\\_work\\testConfigFiles\\config1.txt");
 			br = new BufferedReader(new FileReader(f));
 			String line = null;  
 			String platform = "Android"; // Android or IOS
