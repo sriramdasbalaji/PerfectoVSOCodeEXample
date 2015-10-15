9cf65b3b8e21ad8e64b904b200cf27a1073ab7a4
Uzi Eilon <PerfectoMobile256@gmail.com>
2015-10-15 16:20:40 -0400

    Update util.java

--- src/test/java/util.java
+++ src/test/java/util.java
@@ -235,7 +235,7 @@ public class util {
 		System.out.println("EXECUET TEST BUILD THE LIST FROM THE FILE ");
 		BufferedReader br;
 		try {
-			File f = new File("..\\..\\config1.txt");
+			File f = new File(".."+File.separator +".."+File.separator +"config1.txt");
 			//File f = new File("C:\\aaa\\MS\\BuildAgent\\_work\\testConfigFiles\\config1.txt");
 			br = new BufferedReader(new FileReader(f));
 			String line = null;  
