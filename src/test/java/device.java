4a00964bdfd471e9f43b7b47bc6d8d0bc6ab7186
Uzi Eilon <PerfectoMobile256@gmail.com>
2015-10-28 11:42:13 -0400

    Create device.java

---
+++ src/test/java/device.java
@@ -0,0 +1,14 @@
+package test.java;
+
+public class device {
+	
+	String _id;
+	String _os;
+	String _osVersion;
+	public device(String id,String os,String osVersion)
+	{
+		_id=id;
+		_os =os;
+		_osVersion = osVersion;
+	}
+}
