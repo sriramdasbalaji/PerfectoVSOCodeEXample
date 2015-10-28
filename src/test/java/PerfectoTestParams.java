83b94a6abaadc983217ab64b5a1116737a382020
Uzi Eilon <PerfectoMobile256@gmail.com>
2015-10-28 12:07:06 -0400

    Update PerfectoTestParams.java

--- src/test/java/PerfectoTestParams.java
+++ src/test/java/PerfectoTestParams.java
@@ -3,14 +3,14 @@ package test.java;
 public class PerfectoTestParams {
 
 	String _repKey;
-	String _device;
+	device _device;
 	String _platform;
 	String _activityBandle;
 	String _cloud = "https://demo.perfectomobile.com";
 	String _user = "uzie@perfectomobile.com";
 	String _password = "@Perfecto1";
 
-	public PerfectoTestParams(String device,String repKey,String platform,String ActivityBandle) {
+	public PerfectoTestParams(device device,String repKey,String platform,String ActivityBandle) {
 		_device = device;
 		_repKey = repKey;
 		_platform =platform;
